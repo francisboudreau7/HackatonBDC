8869986f9c317d35c678d784702badeeb1ecd634
alexjuliencodes <alexandre.julien@live.ca>
2019-03-16 16:45:27 -0400

    Random questions

--- questionsSondage.js
+++ questionsSondage.js
@@ -2,22 +2,22 @@ questions = []
 //Banque de questions pour construire le sondage
 qBank = [];
 
-qBank.add(applicationCache.locals.data.questions.presale.firstContact[(Math.random()*10%3)]);
+qBank.add(app.locals.data.questions.presale.firstContact[(Math.random()*10%3)]);
 
 // Lead vient du site web
 if (lead_source.toLowerCase == "website") {
   //Ajouter les questions webOrigin
-  qBank.add(applicationCache.locals.data.questions.presale.webOrigin[(Math.random()*10%2)]);
+  qBank.add(app.locals.data.questions.presale.webOrigin[(Math.random()*10%2)]);
 }
 // Il y a eu une vente
 if (lead_status.toLowerCase == "Sold") {
-  qBank.add(applicationCache.locals.data.questions.sale.completed[(Math.random()*10%2)]);
-  qBank.add(applicationCache.locals.data.questions.postsale.vehiculeAppreciation[(Math.random()*10%2)]);
-  qBank.add(applicationCache.locals.data.questions.postsale.futureLeads[(Math.random()*10%2)]);
+  qBank.add(app.locals.data.questions.sale.completed[(Math.random()*10%2)]);
+  qBank.add(app.locals.data.questions.postsale.vehiculeAppreciation[(Math.random()*10%2)]);
+  qBank.add(app.locals.data.questions.postsale.futureLeads[(Math.random()*10%2)]);
 }
 //Pas eu de vente
 if (lead_status.toLowerCase == "Sleeping") {
-  qBank.add(applicationCache.locals.data.questions.sale.aborted[(Math.random()*10%2)]);
+  qBank.add(app.locals.data.questions.sale.aborted[(Math.random()*10%2)]);
 }
 
 //Choisir les questions pour le sondage
@@ -30,19 +30,19 @@ sondages = [
   {"1":"Prix",3:"Oui"}
 ];
 ansSurvey = {
-  0:{"Oui":10,"Non":0}, 
-  1:{"Service":10,"Prix":0, "Proximité":0}, 
-  2:{"Service":10,"Prix":0, "Proximité":0}, 
-  3:{"Oui":0,"Non":0}, 
-  4:{"Oui":0,"Non":0}, 
-  5:{"Oui":10,"Non":0}, 
-  6:{"Oui":0,"Non":0}, 
-  7:{"Oui":10,"Non":0}, 
-  8:{"Oui":0,"Non":0}, 
-  9:{"Oui":0,"Non":0}, 
-  10:{"Oui":0,"Non":0}, 
-  11:{"Sedan":0,"Vus":0,"Sport":0,"Minifourgonnette":0}, 
-  12:{"Certainement":0, "Probablement":0, "Probablement pas":0,"Certainement pas":0}, 
+  0:{"Oui":10,"Non":0},
+  1:{"Service":10,"Prix":0, "Proximité":0},
+  2:{"Service":10,"Prix":0, "Proximité":0},
+  3:{"Oui":0,"Non":0},
+  4:{"Oui":0,"Non":0},
+  5:{"Oui":10,"Non":0},
+  6:{"Oui":0,"Non":0},
+  7:{"Oui":10,"Non":0},
+  8:{"Oui":0,"Non":0},
+  9:{"Oui":0,"Non":0},
+  10:{"Oui":0,"Non":0},
+  11:{"Sedan":0,"Vus":0,"Sport":0,"Minifourgonnette":0},
+  12:{"Certainement":0, "Probablement":0, "Probablement pas":0,"Certainement pas":0},
 };
 trou = Object.keys(ansSurvey);
 for (var i=0; i<trou.length; i++) {
@@ -50,6 +50,6 @@ for (var i=0; i<trou.length; i++) {
   trouTrou = Object.keys(ansSurvey.trou[i]);
   for (var j=0;j<trouTrou.length;j++) {
     console.log(Object.keys(trouTrou[j]));
-  } 
+  }
 }
 console.log(Object.keys(ansSurvey));
--- routes/surveys.js
+++ routes/surveys.js
@@ -5,11 +5,31 @@ const router = express.Router();
 
 router.get('/new', function (req, res) {
     //TODO Fetch questions and feed it to view
-    let questions = req.app.locals.data.questions["presale"]["firstContact"];
+
+    let questions = req.app.locals.data.questions
     //console.log(questions);
 
+    // Bootleg randomiser for questions
+    let randomQ = [
+        questions["presale"]["firstContact"],
+        questions["presale"]["webOrigin"],
+        questions["sale"]["completed"],
+        questions["sale"]["aborted"],
+        questions["postsale"]["vehiculeAppreciation"],
+        questions["postsale"]["futureLeads"]
+    ]
+
+    // SALE ! to put in utility class ?
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    let randIndex = getRandomInt(0, randomQ.length - 1)
+
     res.setHeader('Content-Type', 'text/html');
-    res.render('newSurvey', { data: questions });
+    res.render('newSurvey', { data: randomQ[randIndex] });
 });
 
 router.get('/completed', function (req, res) {
