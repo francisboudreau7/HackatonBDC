e79c934791e957b6204e0b3776ef86887887c2e9
PAVaillancourt <pierreavaillancourt@gmail.com>
2019-03-16 12:38:05 -0400

    Ajoute fichier question.js

---
+++ questions.js
@@ -0,0 +1,14 @@
+var questions = {
+  "prevente":{
+    "vitrine":["q1","q2"],
+    "subcat2":["q1","q2"],
+  },
+  "vente":{
+    "subcat1":["q1","q2"],
+    "subcat2":["q1","q2"],
+  },
+  "postvente":{
+    "subcat1":["q1","q2"],
+    "subcat2":["q1","q2"],
+  }
+}
